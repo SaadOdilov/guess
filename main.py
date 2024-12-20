a4c335c8adcdb9b59ddc14bfc0a5772d2255f7b5
Saad <saad@gmail.com>
2024-12-20 06:59:33 +0100

    repeat unitila payer wins

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random
 n = random.randit(0, 10)
 guess = int(input("guess a anumber between 0 and 10:"))
 
-if n == guess:
-    print("Toptingiz")
-else:
-    print("Topa olmadingiz")
+
+while:   
+   if n == guess:
+        print("Toptingiz !!!")
+        break
