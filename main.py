664bd6d25f7c2cc3b4d7c2df9c3c671563902832
root <root@vmi1909174.contaboserver.net>
2024-12-22 08:32:53 +0100

    limit number of guesses

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ n = random.randit(0, 10)
 guess = int(input("guess a anumber between 0 and 10:"))
 
 
-while:   
+while True:   
    if n == guess:
         print("Toptingiz !!!")
+        break
+
+    if guess >= 3:
+        print("Yutqazdingiz")
         break
